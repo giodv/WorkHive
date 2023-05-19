75f02f1d9dd6927e8db28a74336bbd5de07488e2
Edoardo Arlati <edoardo.arlati@frontiersin.org>
2023-05-19 14:49:26 +0200

    feat: removed logging

--- src/WorkHive.Server/Program.cs
+++ src/WorkHive.Server/Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
-app.Logger.LogInformation("test");
 // Configure the HTTP request pipeline.
 app.MapGrpcService<EventService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
@@ -23,6 +22,5 @@ app.MapGet("/", () => "Communication with gRPC endpoints must be made through a 
 MigrationsManager.RunMigrations(app.Services, app.Configuration);
 
 app.Run();
-
 
 public partial class Program { }
